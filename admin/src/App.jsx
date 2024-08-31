cc86aa4d42360bdcd02dc79119a7b85a2b9a49c3
sapphire2207 <118914178+sapphire2207@users.noreply.github.com>
2024-08-31 15:04:43 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = 'https://tomato-food-delivery-website-backend.onrender.com/'
+  const url = 'https://tomato-food-delivery-website-backend.onrender.com'
 
   return (
     <div>
