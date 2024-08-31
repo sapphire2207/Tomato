497668d368ca14aa24e1f3e3485a99fbaa9525d6
sapphire2207 <118914178+sapphire2207@users.noreply.github.com>
2024-08-31 16:19:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://tomato-food-delivery-website-backend.onrender.com/'
+export const url = 'https://tomato-food-delivery-website-backend.onrender.com'
